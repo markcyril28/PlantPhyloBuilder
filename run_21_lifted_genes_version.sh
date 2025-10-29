20b2f831c200aa467a2fcb78561e8752391d10c5
markcyril28 <mercadomarkcyril28@gmail.com>
2025-10-29 11:24:10 +0800

    Separate scripts for different versions.

---
+++ run_21_lifted_genes_version.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+input_group="21_lifted_genes_version"
+
+mkdir -p logs
+TIMESTAMP=$(date +%Y%m%d_%H%M%S)
+TIME_LOG="logs/phylo_pipeline_${TIMESTAMP}_${input_group}_script_time_log.log"
+
+# Run with timing
+/usr/bin/time -v bash generate_Alignment_and_Phylo.sh --group "$input_group" 2>> "$TIME_LOG"
